fba522551c2abd28ef7c2701356e190c8cee75c3
JEEYEON LEE <joeuni94@gmail.com>
2026-03-25 00:04:08 +0900

    add '/'

--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,9 @@ from .views import Join, Login, LogOut, UploadProfile
 from django.urls import path
 
 urlpatterns = [
-    path('join', Join.as_view()),
-    path('login', Login.as_view()),
-    path('logout', LogOut.as_view()),
-    path('profile/upload', UploadProfile.as_view())
+    path('join/', Join.as_view()),
+    path('login/', Login.as_view()),
+    path('logout/', LogOut.as_view()),
+    path('profile/upload/', UploadProfile.as_view())
 
 ]
