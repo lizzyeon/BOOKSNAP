81c24953eea63612e0bc1fb6f69c9c7a71241cda
JEEYEON LEE <joeuni94@gmail.com>
2026-03-31 18:40:34 +0900

    contet/admin.py 테스트 설정

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Feed, Like, Reply, Bookmark, Follow
 
-# Register your models here.
+admin.site.register(Feed)
+admin.site.register(Like)
+admin.site.register(Reply)
+admin.site.register(Bookmark)
+admin.site.register(Follow)
