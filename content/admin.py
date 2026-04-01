806a80224446b94ac33fc9c74ab217f4f4f4ea1a
JEEYEON LEE <joeuni94@gmail.com>
2026-04-01 15:53:29 +0900

    admin 설정(완)

--- content/admin.py
+++ content/admin.py
@@ -1,22 +1,38 @@
 from django.contrib import admin
 from .models import Feed, Like, Reply, Bookmark, Follow
+from django.utils.html import format_html
+
 
 @admin.register(Feed)
 class FeedAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'feed')
+    list_display = ('content', 'image_preview', 'email')
+    search_fields = ('email', 'content')
+    ordering = ('-id',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="/media/{}" width="80" height="80" style="object-fit:cover;" />',
+                obj.image
+            )
+        return "-"
+
+    image_preview.short_description = 'Image'
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'feed')
+    list_display = ('feed_id', 'email', 'is_like')
 
 @admin.register(Reply)
 class ReplyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'feed')
+    list_display = ('feed_id', 'email', 'reply_content')
 
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'feed')
+    list_display = ('feed_id', 'email', 'is_marked')
 
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('id', 'from_user', 'to_user')
+    list_display = ('from_user', 'to_user', 'is_followed')
+    search_fields = ('from_user__email', 'to_user__email')
+    list_filter = ('is_followed',)
--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from content.models import Follow
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'last_login', 'email', 'nickname', 'name', 'is_staff', 'is_superuser')
+    list_display = ('id', 'last_login', 'profile_preview', 'email', 'nickname', 'name', 'follower_count', 'following_count', 'is_staff', 'is_superuser')
     search_fields = ('email', 'nickname', 'name')
     list_filter = ('is_staff', 'is_superuser')
     ordering = ('id',)
@@ -21,3 +23,20 @@ class UserAdmin(admin.ModelAdmin):
             'fields': ('is_staff', 'is_superuser', 'is_admin')
         }),
     )
+
+    def profile_preview(self, obj):
+        if obj.profile_image:
+            return format_html(
+                '<img src="/media/{}" width="60" height="60" style="object-fit:cover;" />',
+                obj.profile_image
+            )
+        return "-"
+
+    profile_preview.short_description = 'Profile'
+
+
+    def follower_count(self, obj):
+        return Follow.objects.filter(to_user=obj, is_followed=True).count()
+
+    def following_count(self, obj):
+        return Follow.objects.filter(from_user=obj, is_followed=True).count()
